0c679f07258a86b51ae0d3c56782c6a3206bc9ae
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-09-14 15:23:48 +0700

    make proto for laptop server

---
+++ service/laptop_server_test.go
@@ -0,0 +1,85 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xvbnm48/go-user-management/pb"
+	"github.com/xvbnm48/go-user-management/sample"
+	"github.com/xvbnm48/go-user-management/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestServerCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopNoID := sample.NewLaptop()
+	laptopNoID.Id = ""
+	laptopInvalidID := sample.NewLaptop()
+	laptopInvalidID.Id = "invalid-uuid"
+	laptopDuplicateID := sample.NewLaptop()
+	storeDuplicateID := service.NewInMemoryLaptopStore()
+	err := storeDuplicateID.Save(laptopDuplicateID)
+	require.Nil(t, err)
+
+	testCase := []struct {
+		name   string
+		laptop *pb.Laptop
+		store  service.LaptopStore
+		code   codes.Code
+	}{
+		{
+			name:   "success_with_id",
+			laptop: sample.NewLaptop(),
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.OK,
+		},
+		{
+			name:   "success_no_id",
+			laptop: laptopNoID,
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.InvalidArgument,
+		},
+		{
+			name:   "failure_duplicate_id",
+			laptop: laptopDuplicateID,
+			store:  storeDuplicateID,
+			code:   codes.OK,
+		},
+		{
+			name:   "success_with_id",
+			laptop: sample.NewLaptop(),
+			store:  service.NewInMemoryLaptopStore(),
+			code:   codes.OK,
+		},
+	}
+
+	for i := range testCase {
+		tc := testCase[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			req := &pb.CreateLaptopRequest{
+				Laptop: tc.laptop,
+			}
+			server := service.NewLaptopServer(tc.store)
+			res, err := server.CreateLaptop(context.Background(), req)
+			if tc.code == codes.OK {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.NotEmpty(t, res.Id)
+				if len(tc.laptop.Id) > 0 {
+					require.Equal(t, tc.laptop.Id, res.Id)
+				}
+			} else {
+				require.Error(t, err)
+				require.Nil(t, res)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, tc.code, st.Code())
+			}
+		})
+	}
+}
